a8a56100d679adf423ad9559ea018a65d03f338c
Macs1324 <max.blank410@gmail.com>
2024-07-08 18:24:03 +0200

    super basic hyprland config

--- home.nix
+++ home.nix
@@ -13,6 +13,28 @@
 			"$mod" = "SUPER";
 			bind = [
 				"$mod, Q, exec, kitty"
+			]
+			++ (
+			# workspaces
+			# binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+			builtins.concatLists (builtins.genList (
+			    x: let
+			      ws = let
+				c = (x + 1) / 10;
+			      in
+				builtins.toString (x + 1 - (c * 10));
+			    in [
+			      "$mod, ${ws}, workspace, ${toString (x + 1)}"
+			      "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+			    ]
+			  )
+			  10)
+		      );
+			bindm = [
+			    # mouse movements
+			    "$mod, mouse:272, movewindow"
+			    "$mod, mouse:273, resizewindow"
+			    "$mod ALT, mouse:272, resizewindow"
 			];
 		};
 	};
