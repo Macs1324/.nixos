1382011297cd9c2f316ceef59d0d14ffe48ac0f2
Macs1324 <max.blank410@gmail.com>
2024-07-10 15:29:21 +0200

    add ripgrep and config alacritty

--- home.nix
+++ home.nix
@@ -21,7 +21,23 @@
     enable = true;
     gitCredentialHelper.enable = true;
   };
-  programs.alacritty.enable = true;
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window.dimensions = {
+        lines = 3;
+        columns = 200;
+      };
+      keyboard.bindings = [
+        {
+          key = "K";
+          mods = "Control";
+          chars = "\\u000c";
+        }
+      ];
+    };
+  };
   services.dunst = {
     enable = true;
     settings = {
