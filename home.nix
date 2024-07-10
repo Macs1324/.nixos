1f0f999cd93cdf0c8eb94b61f5a9816b2b3386b3
Macs1324 <max.blank410@gmail.com>
2024-07-10 11:15:58 +0200

    add eww config boilerplate

--- home.nix
+++ home.nix
@@ -5,6 +5,7 @@
 }: {
   imports = [
     ./home/hyprland.nix
+    ./home/eww.nix
   ];
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
---
+++ home/eww.nix
@@ -0,0 +1,11 @@
+{
+  config,
+  pkgs,
+  pc,
+  ...
+}: {
+  programs.eww = {
+    enable = true;
+    configDir = ./eww;
+  };
+}
