b96d5231b7927b4d39e300fea82d43bfa9092c94
Macs1324 <max.blank410@gmail.com>
2024-07-19 09:42:16 +0200

    flake update

--- configuration.nix
+++ configuration.nix
@@ -84,7 +84,7 @@
       nvidiaSettings = true;
 
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.production;
     }
     else {};
 
