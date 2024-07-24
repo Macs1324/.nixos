b4b86a8941ce34b8e98b471dc8d5682cd8f813ce
Macs1324 <max.blank410@gmail.com>
2024-07-24 10:40:04 +0200

    make it ignore the pc file

--- switch.sh
+++ switch.sh
@@ -1,10 +1,12 @@
 alejandra .
 cp /etc/nixos/hardware-configuration.nix .
 git add hardware-configuration.nix
+git add pc
 
 sudo nixos-rebuild switch --flake .#nixos &&
 home-manager switch --flake . &&
 
 git rm hardware-configuration.nix --cached
+git rm pc --cached
 rm hardware-configuration.nix
 lazygit
