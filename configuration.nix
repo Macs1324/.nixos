e43c2e38a5f95185ac03a9a53ce50006d35daf19
Macs1324 <max.blank410@gmail.com>
2024-07-30 14:48:51 +0200

    disable firewall

--- configuration.nix
+++ configuration.nix
@@ -260,7 +260,7 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
   nix.settings.experimental-features = ["nix-command" "flakes"];
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
