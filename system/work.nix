77c2607294aa815ead3f7cf9153cc77e901c25e3
Macs1324 <max.blank410@gmail.com>
2024-08-01 13:40:31 +0200

    remove network config

--- system/work.nix
+++ system/work.nix
@@ -24,15 +24,4 @@
     enable = true;
     port = 6379;
   };
-
-  networking = {
-    interfaces.wlp0s20f3.ipv4.addresses = [
-      {
-        address = "192.168.1.32";
-        prefixLength = 24;
-      }
-    ];
-    defaultGateway = "192.168.1.1";
-    nameservers = ["8.8.8.8"];
-  };
 }
