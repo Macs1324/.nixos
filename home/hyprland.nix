6f09d8ba155819e45090a8149871eda0e15a11b3
Macs1324 <max.blank410@gmail.com>
2024-10-06 13:05:59 +0200

    set homedesktop wallpaper 2 properly

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -192,10 +192,10 @@
       settings = {
         ipc = "on";
         splash = "false";
-        preload = ["${wallpaper}-1.png" "${wallpaper}-2.png"];
+        preload = ["${wallpaper}-1.png" "${wallpaper}-2.jpg"];
         wallpaper = [
           "DP-2,${wallpaper}-1.png"
-          "HDMI-A-2,${wallpaper}-2.png"
+          "HDMI-A-2,${wallpaper}-2.jpg"
         ];
       };
     }
