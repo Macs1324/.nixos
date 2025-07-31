932fbcaeb4935c8816c8eeaca5101619559287c8
Macs1324 <max.blank410@gmail.com>
2025-07-31 12:31:29 +0200

    nvim-tree keybinds

--- home/nvim/modules/file-explorer.nix
+++ home/nvim/modules/file-explorer.nix
@@ -17,6 +17,25 @@
   plugins.nvim-tree = {
     enable = true;
 
+    onAttach = {
+      __raw = ''
+        function(bufnr)
+          local api = require "nvim-tree.api"
+
+          local function opts(desc)
+            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+          end
+
+          -- Use l to enter directory or open file
+          vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+          -- Use h to close directory
+          vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+          -- Use z to collapse all directories
+          vim.keymap.set('n', 'z', api.tree.collapse_all, opts('Collapse All'))
+        end
+      '';
+    };
+
     openOnSetup = true;
     disableNetrw = true;
     hijackNetrw = true;
