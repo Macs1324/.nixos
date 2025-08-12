5fb71b16b3086cec52a22885c3ad29e20ebd9927
Macs1324 <max.blank410@gmail.com>
2025-08-12 09:20:33 +0200

    fix nixvim file-explorer warnings

--- home/nvim/modules/file-explorer.nix
+++ home/nvim/modules/file-explorer.nix
@@ -17,91 +17,88 @@
   plugins.nvim-tree = {
     enable = true;
 
-    onAttach = {
-      __raw = ''
-        function(bufnr)
-          local api = require "nvim-tree.api"
-
-          local function opts(desc)
-            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-          end
-
-          -- Use l to enter directory or open file
-          vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
-          vim.keymap.set('n', '<cr>', api.node.open.edit, opts('Open'))
-          -- Use h to close directory
-          vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
-          -- Use z to collapse all directories
-          vim.keymap.set('n', 'z', api.tree.collapse_all, opts('Collapse All'))
-          -- Use a to add file
-          vim.keymap.set('n', 'a', api.fs.create, opts('Create File/Directory'))
-          -- Vim-style file management
-          vim.keymap.set('n', 'y', api.fs.copy.node, opts('Copy'))
-          vim.keymap.set('n', 'x', api.fs.cut, opts('Cut'))
-          vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
-          vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
-          vim.keymap.set('n', 'd', api.fs.remove, opts('Delete'))
-        end
-      '';
-    };
-
     openOnSetup = true;
-    disableNetrw = true;
-    hijackNetrw = true;
-    hijackCursor = true;
-    hijackUnnamedBufferWhenOpening = true;
-    syncRootWithCwd = true;
+    settings = {
+      disable_netrw = true;
+      hijack_netrw = true;
+      hijack_cursor = true;
+      hijack_nnamed_buffer_when_opening = true;
+      sync_root_with_cwd = true;
+      updateFocusedFile.enable = true;
+      sort_by = "case_sensitive";
 
-    updateFocusedFile.enable = true;
+      filters = {
+        custom = ["__pycache__"];
+        exclude = [];
+      };
+      git = {
+        enable = true;
+        ignore = true;
+      };
+      view = {
+        side = "right";
+        width = 30;
+        number = true;
+        relativenumber = true;
+        preserveWindowProportions = true;
+      };
+      renderer = {
+        rootFolderLabel = false;
+        indentWidth = 2;
+        highlightGit = true;
+        indentMarkers.enable = true;
 
-    sortBy = "case_sensitive";
-
-    filters = {
-      custom = ["__pycache__"];
-      exclude = [];
-    };
-
-    git = {
-      enable = true;
-      ignore = true;
-    };
-
-    view = {
-      side = "right";
-      width = 30;
-      number = true;
-      relativenumber = true;
-      preserveWindowProportions = true;
-    };
-
-    renderer = {
-      rootFolderLabel = false;
-      indentWidth = 2;
-      highlightGit = true;
-      indentMarkers.enable = true;
-
-      icons.glyphs = {
-        default = "󰈚";
-        symlink = "";
-        folder = {
-          default = "";
-          empty = "";
-          emptyOpen = "";
-          open = "";
-          symlink = "";
-          symlinkOpen = "";
-          arrowOpen = "";
-          arrowClosed = "";
+        icons.glyphs = {
+          default = "󰈚";
+          symlink = "";
+          folder = {
+            default = "";
+            empty = "";
+            emptyOpen = "";
+            open = "";
+            symlink = "";
+            symlinkOpen = "";
+            arrowOpen = "";
+            arrowClosed = "";
+          };
+          git = {
+            unstaged = "✗";
+            staged = "✓";
+            unmerged = "";
+            renamed = "➜";
+            untracked = "★";
+            deleted = "";
+            ignored = "◌";
+          };
         };
-        git = {
-          unstaged = "✗";
-          staged = "✓";
-          unmerged = "";
-          renamed = "➜";
-          untracked = "★";
-          deleted = "";
-          ignored = "◌";
-        };
+      };
+
+      onAttach = {
+        __raw = ''
+          function(bufnr)
+            local api = require "nvim-tree.api"
+
+            local function opts(desc)
+              return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+            end
+
+            -- Use l to enter directory or open file
+            vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+            vim.keymap.set('n', '<cr>', api.node.open.edit, opts('Open'))
+            -- Use h to close directory
+            vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+            -- Use z to collapse all directories
+            vim.keymap.set('n', 'z', api.tree.collapse_all, opts('Collapse All'))
+            -- Use a to add file
+            vim.keymap.set('n', 'a', api.fs.create, opts('Create File/Directory'))
+            -- Vim-style file management
+            vim.keymap.set('n', 'y', api.fs.copy.node, opts('Copy'))
+            vim.keymap.set('n', 'x', api.fs.cut, opts('Cut'))
+            vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
+            vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
+            vim.keymap.set('n', 'd', api.fs.remove, opts('Delete'))
+          end
+        '';
       };
     };
   };
