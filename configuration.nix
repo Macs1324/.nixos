e6f82e22eb33dcdebef2d6677400f55b0fedfb24
Macs1324 <max.blank410@gmail.com>
2025-08-13 08:00:44 +0200

    add bun

--- configuration.nix
+++ configuration.nix
@@ -10,23 +10,21 @@
   stylix,
   pcs,
   ...
-}: {
-  imports =
-    [
-      # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ]
-    ++ {
-      "homedesktop" = [
-        ./system/games.nix
-        ./system/work.nix
-      ];
-      "workdesktop" = [./system/work.nix];
-      "worklaptop" = [./system/work.nix];
-    }
-  .${
-      pc
-    };
+}:
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ]
+  ++ {
+    "homedesktop" = [
+      ./system/games.nix
+      ./system/work.nix
+    ];
+    "workdesktop" = [ ./system/work.nix ];
+    "worklaptop" = [ ./system/work.nix ];
+  }
+  .${pc};
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -63,60 +61,63 @@
   hardware.uinput.enable = true;
   # Enable OpenGL
   hardware.graphics =
-    if pc == "homedesktop"
-    then {
-      enable = true;
-      enable32Bit = true;
-      extraPackages = with pkgs; [
-        rocmPackages.clr.icd
-      ];
-    }
-    else {
-      enable = true;
-    };
+    if pc == "homedesktop" then
+      {
+        enable = true;
+        enable32Bit = true;
+        extraPackages = with pkgs; [
+          rocmPackages.clr.icd
+        ];
+      }
+    else
+      {
+        enable = true;
+      };
 
   hardware.nvidia =
-    if pc == "workdesktop"
-    then {
-      # Modesetting is required.
-      modesetting.enable = true;
+    if pc == "workdesktop" then
+      {
+        # Modesetting is required.
+        modesetting.enable = true;
 
-      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-      # Enable this if you have graphical corruption issues or application crashes after waking
-      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-      # of just the bare essentials.
-      powerManagement.enable = false;
+        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+        # Enable this if you have graphical corruption issues or application crashes after waking
+        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+        # of just the bare essentials.
+        powerManagement.enable = false;
 
-      # Fine-grained power management. Turns off GPU when not in use.
-      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-      powerManagement.finegrained = false;
+        # Fine-grained power management. Turns off GPU when not in use.
+        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+        powerManagement.finegrained = false;
 
-      # Use the NVidia open source kernel module (not to be confused with the
-      # independent third-party "nouveau" open source driver).
-      # Support is limited to the Turing and later architectures. Full list of
-      # supported GPUs is at:
-      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-      # Only available from driver 515.43.04+
-      # Currently alpha-quality/buggy, so false is currently the recommended setting.
-      open = false;
+        # Use the NVidia open source kernel module (not to be confused with the
+        # independent third-party "nouveau" open source driver).
+        # Support is limited to the Turing and later architectures. Full list of
+        # supported GPUs is at:
+        # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+        # Only available from driver 515.43.04+
+        # Currently alpha-quality/buggy, so false is currently the recommended setting.
+        open = false;
 
-      # Enable the Nvidia settings menu,
-      # accessible via `nvidia-settings`.
-      nvidiaSettings = true;
+        # Enable the Nvidia settings menu,
+        # accessible via `nvidia-settings`.
+        nvidiaSettings = true;
 
-      # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-    }
-    else {};
+        # Optionally, you may need to select the appropriate driver version for your specific GPU.
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+      }
+    else
+      { };
 
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers =
-    if pc == "workdesktop"
-    then ["nvidia"]
-    else [
-      "modesetting"
-      # "fbdev"
-    ];
+    if pc == "workdesktop" then
+      [ "nvidia" ]
+    else
+      [
+        "modesetting"
+        # "fbdev"
+      ];
 
   # Increase the amount of inotify watchers
   # Note that inotify watches consume 1kB on 64-bit machines.
@@ -125,17 +126,15 @@
     "fs.inotify.max_user_instances" = 1024; # default:   128
     "fs.inotify.max_queued_events" = 32768; # default: 16384
   };
-  boot.initrd.kernelModules =
-    if pc == "homedesktop"
-    then ["amdgpu"]
-    else [];
+  boot.initrd.kernelModules = if pc == "homedesktop" then [ "amdgpu" ] else [ ];
 
   systemd.tmpfiles.rules =
-    if pc == "homedesktop"
-    then [
-      "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
-    ]
-    else [];
+    if pc == "homedesktop" then
+      [
+        "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+      ]
+    else
+      [ ];
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
@@ -176,15 +175,15 @@
   services.libinput.enable = true;
   services.postgresql = {
     enable = true;
-    ensureDatabases = ["clockout"];
+    ensureDatabases = [ "clockout" ];
     authentication = pkgs.lib.mkOverride 10 ''
       #type database  DBuser  auth-method
       local all       all     trust
     '';
   };
 
-  users.groups.uinput = {};
-  users.groups.plugdev = {};
+  users.groups.uinput = { };
+  users.groups.plugdev = { };
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.macs = {
     isNormalUser = true;
@@ -305,6 +304,7 @@
 
     # Programming languages
     nodejs
+    bun
     rustup
     python3
     zig
@@ -441,8 +441,8 @@
       "flakes"
     ];
 
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
