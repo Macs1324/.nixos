9cf00b9e8ced78a14c4290883d5aa951e578abaa
Macs1324 <max.blank410@gmail.com>
2025-09-25 12:55:03 +0200

    switch to GRUB

--- configuration.nix
+++ configuration.nix
@@ -29,8 +29,11 @@
     };
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
   boot.loader.timeout = null;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixmacs"; # Define your hostname.
