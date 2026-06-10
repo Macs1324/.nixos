391e5ef89a07bada483997c0cfde50d26960dde7
Macs1324 <max.blank410@gmail.com>
2026-06-10 13:49:13 +0200

    fix up noctalia flake input

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     niri.inputs.nixpkgs.follows = "nixpkgs";
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell/v5";
+      url = "github:noctalia-dev/noctalia";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
