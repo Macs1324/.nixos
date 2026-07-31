3da77a7831ec2403042537032f018167ce060caf
Macs1324 <max.blank410@gmail.com>
2026-07-31 17:06:09 +0200

    add quarter column width presets
    u

--- home/niri.nix
+++ home/niri.nix
@@ -213,9 +213,11 @@
         background-color = "transparent";
 
         preset-column-widths = [
+          {proportion = 0.25;}
           {proportion = 0.33333;}
           {proportion = 0.5;}
           {proportion = 0.66667;}
+          {proportion = 0.75;}
         ];
 
         default-column-width = {
