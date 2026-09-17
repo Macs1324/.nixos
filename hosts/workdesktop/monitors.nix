4a968ffe1c8d32b330a4aa22aef5ea0f32c314bc
Macs1324 <max.blank410@gmail.com>
2026-09-17 12:13:29 +0200

    try out gruvbox on workdesktop

--- hosts/workdesktop/monitors.nix
+++ hosts/workdesktop/monitors.nix
@@ -10,8 +10,8 @@
       x = 0;
       y = 0;
     };
-    wallpaper = ../../assets/wallpapers/forest-sunset.png;
-    hyprlandWorkspaces = ["1"];
+    wallpaper = ../../assets/wallpapers/gruvbox-astronaut.png;
+    hyprlandWorkspaces = [ "1" ];
   };
   HDMI-A-2 = {
     mode = {
@@ -23,7 +23,7 @@
       x = 3440;
       y = 0;
     };
-    wallpaper = ../../assets/wallpapers/night-mountains.png;
-    hyprlandWorkspaces = ["2"];
+    wallpaper = ../../assets/wallpapers/gruvbox-pixels.png;
+    hyprlandWorkspaces = [ "2" ];
   };
 }
