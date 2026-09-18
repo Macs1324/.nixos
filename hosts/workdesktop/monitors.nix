e681d9e0058d589a708fc2a341e0952e44396e04
Macs1324 <max.blank410@gmail.com>
2026-09-18 15:09:48 +0200

    workdesktop: change wallpapers

--- hosts/workdesktop/monitors.nix
+++ hosts/workdesktop/monitors.nix
@@ -10,7 +10,7 @@
       x = 0;
       y = 0;
     };
-    wallpaper = ../../assets/wallpapers/gruvbox-astronaut.png;
+    wallpaper = ../../assets/wallpapers/night-mountains.png;
     hyprlandWorkspaces = [ "1" ];
   };
   HDMI-A-2 = {
@@ -23,7 +23,7 @@
       x = 3440;
       y = 0;
     };
-    wallpaper = ../../assets/wallpapers/gruvbox-pixels.png;
+    wallpaper = ../../assets/wallpapers/forest-sunset.png;
     hyprlandWorkspaces = [ "2" ];
   };
 }
